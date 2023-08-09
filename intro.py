f55a9bf414eb51bd113769f383a1cccf13550fd7
unknown <maheshvmahendrakar@gmail.com>
2023-08-09 21:07:40 +0530

    second day

--- intro.py
+++ intro.py
@@ -1,2 +1,21 @@
 x=10
 print(x)
+# print(x+5)
+# a=10
+# b=20
+# print(a+b)
+
+# y=5.8
+# print(y)
+# print(type(y))
+# print(type(a))
+
+p=4+5j
+print(p)
+print(type(p))
+x=6+10j
+print(x)
+
+q=10
+r=20
+print(q-r)
